c2c825ad781e49b8c460cbeb3f588ac3509282e4
yerine <yenini0206@gmail.com>
2020-03-30 22:03:48 +0900

    update

---
+++ 1914_하노이 탑.py
@@ -0,0 +1,15 @@
+def hanoi(n, from_pos=1, to_pos=3, aux_pos=2):
+    if n == 1:
+        print(from_pos, to_pos)
+        return
+    
+    hanoi(n-1, from_pos, aux_pos, to_pos)
+    
+    print(from_pos, to_pos)
+    
+    hanoi(n-1, aux_pos, to_pos, from_pos)
+
+n = int(input())
+print(2**n-1)
+if n <= 20:
+    hanoi(n)
